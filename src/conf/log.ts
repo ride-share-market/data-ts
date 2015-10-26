bc5598afd776e2e8274b0a23f879eefc22c97b1d
Rudi Starcevic <ooly.me@gmail.com>
2015-10-26 15:15:22 +0800

    Clean up path.join

--- src/conf/log.ts
+++ src/conf/log.ts
@@ -7,13 +7,15 @@ const env: string = process.env.NODE_ENV = process.env.NODE_ENV || 'loc';
 
 import {config} from './config';
 
-export let create = function (name) {
+export let create: any = function (name: string): any {
+
+  let logFileName: string = `${name}_${env}.log`;
 
   let logger: any = bunyan.createLogger({
     name: name,
     streams: [
       {
-        path: path.join(config.get('root'), config.get('logDir')) + '/' + name + '_' + env + '.log'
+        path: path.join(config.get('root'), config.get('logDir'), logFileName)
       }
     ]
   });
